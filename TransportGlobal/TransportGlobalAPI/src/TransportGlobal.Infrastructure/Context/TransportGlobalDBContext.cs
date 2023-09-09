029d86ffff227b52d3a01c071d9f602e64b413f4
ynsmrkrds <ynsmrkrds@gmail.com>
2023-09-09 13:31:28 +0300

    mssql repository base codings done

---
+++ TransportGlobal/TransportGlobalAPI/src/TransportGlobal.Infrastructure/Context/TransportGlobalDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace TransportGlobal.Infrastructure.Context
+{
+    public class TransportGlobalDBContext : DbContext
+    {
+        public TransportGlobalDBContext(DbContextOptions<TransportGlobalDBContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ TransportGlobal/TransportGlobalAPI/src/TransportGlobal.Infrastructure/Extensions/Registrations/ServiceRegistration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+using System.Reflection;
+using TransportGlobal.Domain.Repositories;
+using TransportGlobal.Infrastructure.Context;
+using TransportGlobal.Infrastructure.Repositories;
+
+namespace TransportGlobal.Infrastructure.Extensions.Registrations
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("TransportGlobalDB")!;
+
+            services.AddDbContext<TransportGlobalDBContext>(opt =>
+            {
+                opt.UseSqlServer(connectionString, asm =>
+                {
+                    asm.MigrationsAssembly(Assembly.GetAssembly(typeof(TransportGlobalDBContext))?.GetName().Name);
+                });
+            });
+
+            services.Scan(scan =>
+            {
+                scan.FromAssembliesOf(typeof(Repository<>))
+                    .AddClasses(classes => classes.AssignableTo(typeof(IRepository<>)))
+                    .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+                    .AsImplementedInterfaces()
+                    .WithScopedLifetime();
+            });
+
+            return services;
+        }
+    }
+}
